0eb50b23af2d01b711da5b181eeb3f1ef7506ef2
Gheorghe Bastos <gheorghe.bastos.ofc@gmail.com>
2026-04-22 00:53:24 -0300

    add login page with authentication functionality

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -7,7 +7,14 @@ function autenticacao() {
 </script>
 
 <template>
+
+
+
   <div class="flex flex-col items-center justify-center gap-4 p-4">
+    <UPageHero class="w-4xl" title="SEU ESPAÇO FITNESS" description="Faça calculos de IMC e macronutrientes para manter-se saudável e em forma sem sair de casa." 
+    :ui="{
+      container: 'py-5 sm:py-5 lg:py-5'
+    }"/>
     <UPageCard class=" flex flex-col items-center justify-center w-full max-w-md p-2 w-xl
     light:shadow-[0px_0px_10px]">
       <div class="flex flex-col mb-2">
@@ -26,7 +33,8 @@ function autenticacao() {
         <UInput class="w-full rounded-[0.5rem]" type="password" placeholder="Digite sua senha" />
       </UFormField>
 
-      <UButton color="primary" variant="solid" class="w-full flex items-center justify-center" @click="autenticacao">Entrar</UButton>
+      <UButton color="primary" variant="solid" class="w-full flex items-center justify-center" @click="autenticacao">
+        Entrar</UButton>
 
     </UPageCard>
   </div>
