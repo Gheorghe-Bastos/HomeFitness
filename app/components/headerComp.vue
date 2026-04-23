c1cfa662dea15e61baccd083e5b31ce396066633
Gheorghe Bastos <gheorghe.bastos.ofc@gmail.com>
2026-04-23 01:21:09 -0300

    update favicon to SVG format and adjust header layout

--- app/components/headerComp.vue
+++ app/components/headerComp.vue
@@ -5,8 +5,10 @@
 <template>
   <UHeader>
     <template #left>
-      <div class="flex gap-0">
-        <h1 class="text-3xl">Home</h1><h1 class="text-3xl text-primary">Fitness</h1>
+      <div class="flex items-center gap-0">
+        <img class="size-[2.3rem]" src="/favicon.svg">
+        <h1 class="text-3xl">Home</h1>
+        <h1 class="text-3xl text-primary">Fitness</h1>
       </div>
     </template>
 
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -8,24 +8,25 @@ const erroDescription = inject("erroDescription");
 const emailInput = ref('');
 const senhaInput = ref('');
 const cadastrar = ref(false);
+const toast = useToast();
 
-function autenticacao() {
+async function autenticacao() {
   console.log('Autenticando usuário...')
   erro.value = false;
-  erroDescription.value = '';
-  const userEmail = emailInput.value;
-  const userPassword = senhaInput.value;
+  const userEmail = emailInput.value.trim();
+  const userPassword = senhaInput.value.trim();
 
-  if (!userEmail || !userPassword) {
+
+  if (userEmail === '' || userPassword === '') {
     erro.value = true;
     erroDescription.value = 'Por favor, preencha todos os campos.';
 
-    return false;
+    return;
   }
 
-  const usuarioEncontrado = usuarios.find(u => u.email === userEmail);
+  const usuarioEncontrado = usuarios.value.find(u => u.email === userEmail);
 
-  if (!usuarioEncontrado || usuarioEncontrado.senha !== userPassword) {
+  if (!usuarioEncontrado) {
     erro.value = true;
     erroDescription.value = 'Usuário inválido, tente novamente.';
     emailInput.value = '';
@@ -43,20 +44,49 @@ function autenticacao() {
 
       return;
     }
+
     console.log('Usuário autenticado com sucesso:', usuarioEncontrado);
-
-    const usuarioNovo = {
-      email: userEmail,
-      senha: userPassword
-    }
-
-    usuarios.push(usuarioNovo);
 
     emailInput.value = '';
     senhaInput.value = '';
 
-    navigateTo('/abaFitness');
+    await navigateTo('/abaFitness');
   }
+}
+
+async function cadastro() {
+  erro.value = false;
+  const userEmail = emailInput.value.trim();
+  const userPassword = senhaInput.value;
+
+  if (!userEmail || !userPassword) {
+    erro.value = true;
+    erroDescription.value = 'Por favor, preencha todos os campos.';
+
+    return;
+  };
+
+  const usuarioExiste = usuarios.value.find(u => u.email === userEmail);
+
+  if (usuarioExiste) {
+    erro.value = true;
+    erroDescription.value = 'Este email já está cadastrado. Por favor, use outro email.';
+
+    return false;
+  }
+
+  const novoUsuario = {
+    email: userEmail,
+    senha: userPassword
+  };
+
+  usuarios.value.push(novoUsuario);
+
+  console.log(usuarios.value.at(-1))
+
+  toast.add({ title: 'Login bem sucedido', description: 'Redirecionando para a pagina principal...' })
+
+  await navigateTo('/abaFitness');
 }
 
 </script>
@@ -67,12 +97,11 @@ function autenticacao() {
   <div class="flex flex-col items-center justify-center gap-4 p-4">
     <UPageHero class="w-4xl" title="SEU ESPAÇO FITNESS"
       description="Faça calculos de IMC e macronutrientes para manter-se saudável e em forma sem sair de casa." :ui="{
-        container: 'py-5 sm:py-5 lg:py-5'
+        container: 'py-5 sm:py-5 lg:py-3'
       }" />
 
     <UPageCard v-if="cadastrar" class=" flex flex-col items-center justify-center w-full max-w-md p-2 w-xl
-    light:shadow-[0px_0px_10px]"
-    :ui="{ container: 'gap-x-6 gap-y-2'}">
+    light:shadow-[0px_0px_10px]" :ui="{ container: 'gap-x-6 gap-y-3' }">
       <div class="flex flex-col mb-2">
         <UIcon name="line-md:account" class="w-12 h-12 text-neutral mx-auto" />
         <div class="flex flex-col justify-center items-center">
@@ -81,30 +110,33 @@ function autenticacao() {
         </div>
       </div>
 
-      <p v-if="erro" class="flex justify-center text-sm text-red-400">{{ erroDescription }}</p>
 
-      <UFormField label="Email">
-        <UInput v-model="emailInput" class="w-full" type="email" placeholder="Digite seu email" />
-      </UFormField>
+      <UForm class="flex flex-col gap-3" @submit="cadastro">
 
-      <UFormField label="Senha">
-        <UInput v-model="senhaInput" class="w-full rounded-[0.5rem]" type="password" placeholder="Digite sua senha" />
-      </UFormField>
+        <p v-if="erro" class="flex justify-center text-sm text-red-400">{{ erroDescription }}</p>
 
-      <UButton color="primary" variant="solid" class="w-full flex items-center justify-center" @click="cadastro">
-        CADASTRAR</UButton>
+        <UFormField label="Email">
+          <UInput v-model="emailInput" class="w-full" type="email" placeholder="Digite seu email" />
+        </UFormField>
 
-      <div class="flex w-full items-end">
-        <UButton color="neutral" variant="subtle" class="w-full flex items-center justify-center"
-          @click="cadastrar = false">
-          JÁ POSSUO UMA CONTA</UButton>
-      </div>
+        <UFormField label="Senha">
+          <UInput v-model="senhaInput" class="w-full rounded-[0.5rem]" type="password" placeholder="Digite sua senha" />
+        </UFormField>
 
+        <UButton color="primary" variant="solid" class="w-full flex items-center justify-center" @click="cadastro">
+          CADASTRAR</UButton>
+
+        <div class="flex w-full items-end">
+          <UButton color="neutral" variant="subtle" class="w-full flex items-center justify-center"
+            @click="cadastrar = false">
+            JÁ POSSUO UMA CONTA</UButton>
+        </div>
+      </UForm>
     </UPageCard>
 
+
     <UPageCard v-else="cadastrar" class="flex flex-col items-center justify-center w-full max-w-md p-2 w-xl
-      light:shadow-[0px_0px_10px]"
-      :ui="{ container: 'gap-x-6 gap-y-2' }">
+      light:shadow-[0px_0px_10px]" :ui="{ container: 'gap-x-6 gap-y-3' }">
 
       <div class="flex flex-col mb-2">
         <UIcon name="line-md:account" class="w-12 h-12 text-neutral mx-auto" />
@@ -115,25 +147,26 @@ function autenticacao() {
 
       </div>
 
+      <UForm class="flex flex-col gap-3" @submit="autenticacao">
       <p v-if="erro" class="flex justify-center text-sm text-red-400">{{ erroDescription }}</p>
-      
-      <UFormField label="Email">
-        <UInput v-model="emailInput" class="w-full" type="email" placeholder="Digite seu email" />
-      </UFormField>
 
-      <UFormField label="Senha">
-        <UInput v-model="senhaInput" class="w-full rounded-[0.5rem]" type="password" placeholder="Digite sua senha" />
-      </UFormField>
+        <UFormField label="Email">
+          <UInput v-model="emailInput" class="w-full" type="email" placeholder="Digite seu email" />
+        </UFormField>
 
-      <UButton color="primary" variant="solid" class="w-full flex items-center justify-center" @click="autenticacao">
-        ENTRAR</UButton>
+        <UFormField label="Senha">
+          <UInput v-model="senhaInput" class="w-full rounded-[0.5rem]" type="password" placeholder="Digite sua senha" />
+        </UFormField>
 
-      <div class="flex w-full items-end">
-        <UButton color="neutral" variant="subtle" class="w-full flex items-center justify-center"
-          @click="cadastrar = true">
-          CADASTRAR</UButton>
-      </div>
+        <UButton color="primary" variant="solid" class="w-full flex items-center justify-center" @click="autenticacao">
+          ENTRAR</UButton>
 
+        <div class="flex w-full items-end">
+          <UButton color="neutral" variant="subtle" class="w-full flex items-center justify-center"
+            @click="cadastrar = true">
+            CADASTRAR</UButton>
+        </div>
+      </UForm>
     </UPageCard>
   </div>
 </template>
